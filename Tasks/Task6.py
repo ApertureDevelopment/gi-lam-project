01347c32066548e5b8e444b7535c0d4d0bad6c3f
Maximilian Grüttemeier <16450982+ApertureDevelopment@users.noreply.github.com>
2024-06-21 22:21:11 +0200

    Remove alt approach

--- Tasks/Task6.py
+++ Tasks/Task6.py
@@ -15,23 +15,3 @@ def minisort(sortZahlen):
                     sortZahlen[i+1]=zahlA
                     sortZahlen[i]=zahlB
     return sortZahlen
-
-
-# Alternative approach
-def swap(array, iterA, iterB):
-    temp = array[iterA]
-    array[iterA] = array[iterB]
-    array[iterB] = temp
-
-def altSort(array):
-    iter = 0
-    array_length = len(array)
-
-    while iter < array_length:
-        if i + 1 == array_length: return;
-        if array[i] > array[i + 1]:
-            swap(array, i, i + 1)
-            if(i > 0):
-                i = i - 1
-        else:
-            i = i + 1
